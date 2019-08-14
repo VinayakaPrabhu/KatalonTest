8e8ff0785d65b8f6df72db33f5109f70583b0be8
vinayaka.prabhu <vinayaka.prabhu@BLRW-36SF9X2.scientificgames.com>
2019-08-14 12:56:16 +0530

    First commit

---
+++ Test Suites/firsttest.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>firsttest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c611f6d8-52b3-486c-bf26-6d0cdeb25947</testSuiteGuid>
+   <testCaseLink>
+      <guid>555c790b-b14d-4c76-afc8-174377ef5644</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/loginfinallyfinal</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
